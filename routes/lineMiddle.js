159481821f46ee073ffc2ee5c613797738d3b925
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-25 00:31:39 +0700

    complete

--- routes/lineMiddle.js
+++ routes/lineMiddle.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const { Payload } = require('dialogflow-fulfillment');
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
 const LINE_HEADER = {
     "Content-Type": "application/json",
-    Authorization: `Bearer mPTPwCDDSVAujVGodPG9sRtQVD8/dq7ZYpiGNPY0PwSuAkQNYsX5OuH2mxnhXwwq/lAYnj3Lc8lC9oyF3Tu5rJLcoFQCPwNBs1tCnk1X79jfLfuj0SdQZ382z4+TGitYIgXSx9DEAj/x68j5MA5awgdB04t89/1O/w1cDnyilFU=`
+    Authorization: `Bearer OuKKma1JXpkmfskoMpGX3ShiGwRH4gueGfBeWhp2TAiJxZd5lvur11fHgFZkgawY6NTicCmpUuox6cJndDRipXYIubs8qjOysPJHXJKsi4ySedTHevqgKCCivPf3ETWpIuh/iVlcRe68CuC6zsyjHwdB04t89/1O/w1cDnyilFU=`
 }
 router.get('/', async (req, res) => {
 
@@ -12,40 +14,40 @@ router.get('/', async (req, res) => {
   });
 });
 router.post('/', async (req, res) => {
-  console.log(req.body);
   if (req.method === "POST") {
     let event = req.body.events[0]
     if (event.type === "message" && event.message.type === "text") {
       postToDialogflow(req);
     } else {
-      reply(req);
+      reply(event.replyToken, {type: "text", text :event.message.type});
     }
   }
   return res.status(200).send(req.method);
 });
 
   
-  const reply = req => {
-    return request.post({
-      uri: `${LINE_MESSAGING_API}/reply`,
-      headers: LINE_HEADER,
-      body: JSON.stringify({
-        replyToken: req.body.events[0].replyToken,
-        messages: [
-          {
-            type: "text",
-            text: JSON.stringify(req.body)
-          }
-        ]
-      })
-    });
+  const reply = async (replyToken, message) => {
+    try{
+      await axios({
+        url : `${LINE_MESSAGING_API}/reply`,
+        headers : LINE_HEADER,
+        method : "post",
+        data: JSON.stringify({
+          replyToken: replyToken,
+          messages: [message]
+        })
+      });
+    }catch(err){
+      console.log(err);
+    }
   };
-  const postToDialogflow = req => {
-    req.headers.host = "bots.dialogflow.com";
-    return request.post({
-      uri: "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/825c62bf-c875-4b48-a015-3e031c5d346d",
-      headers: req.headers,
-      body: JSON.stringify(req.body)
+  const postToDialogflow =async payloadRequest => {
+    payloadRequest.headers.host = "dialogflow.cloud.google.com";
+    await axios({
+      url : "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/825c62bf-c875-4b48-a015-3e031c5d346d",
+      headers : payloadRequest.headers,
+      method : "post",
+      data: payloadRequest.body
     });
   };
 module.exports = router;
