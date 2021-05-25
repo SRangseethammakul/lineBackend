4452cc422332b8d29de3aa5e163e9bda147dbd4c
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-05-25 22:36:08 +0700

    add spotify api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const lineMessageRouter = require('./routes/lineMessage');
 const lineMiddleRouter = require('./routes/lineMiddle');
+const spotify = require("./controller/spotify");
 const app = express();
 
 app.use(logger('dev'));
@@ -19,5 +20,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/apis/linemessage', lineMessageRouter);
 app.use('/apis/lineMidRouter', lineMiddleRouter);
+app.get("/spotify", (req, res) => {
+    spotify.receivedAuthCode(req.query.code);
+    res.status(200).send("Login Successfully!");
+});
 
 module.exports = app;
